9197804cb8a47117a15452553e7dcb8b47137170
LesLie.W <manyu34@gmail.com>
2023-08-29 12:45:31 +0800

    first commit

---
+++ hkid_face_test/android/app/src/main/kotlin/hk/easco/leslie/hkid_face_test/hkid_face_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package hk.easco.leslie.hkid_face_test.hkid_face_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ hkid_test_2/android/app/src/main/kotlin/hk/eascotel/leslie/hkid/test/hkid_test_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package hk.eascotel.leslie.hkid.test.hkid_test_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
